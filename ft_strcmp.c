b935440f7992858c564245c0a52045a364c01315
sadamant <sharfy.adamantine@gmail.com>
2017-09-09 08:04:09 +0700

    !ft_strcmp
    doesn't support unicode yet

---
+++ ft_strcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/09 06:56:31 by sadamant          #+#    #+#             */
+/*   Updated: 2017/09/09 07:20:01 by sadamant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;	
+	while (s1[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if ((int)s1[i] < (int)s2[i])
+			return (-1);
+		else
+			return (1);
+	}
+	if (ft_strlen(s1) == ft_strlen(s2))
+		return (0);
+	else if (ft_strlen(s1) < ft_strlen(s2))
+		return (-1);
+	else
+		return (1);
+}
