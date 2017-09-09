3b5dd14c57b215c7f3ff3733cfb40c8e83b5b3bf
sadamant <sharfy.adamantine@gmail.com>
2017-09-09 09:25:58 +0700

    ft_putnbr_fd bug fix
    I think

--- test.c
+++ test.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/07 10:06:03 by sadamant          #+#    #+#             */
-/*   Updated: 2017/09/09 09:23:59 by sadamant         ###   ########.fr       */
+/*   Created: 2017/09/09 09:24:18 by sadamant          #+#    #+#             */
+/*   Updated: 2017/09/09 09:24:57 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#define INT_MIN -2147483648
 
-void	ft_putnbr_fd(int n, int fd)
+int	main(void)
 {
-	if (n == INT_MIN)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = n * - 1;
-	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd((n % 10) + '0', fd);
+	ft_putnbr(100123);
 }
