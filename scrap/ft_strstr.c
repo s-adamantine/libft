145c12d9e2008549611a41a221c7a42b2801abe1
sadamant <sharfy.adamantine@gmail.com>
2017-09-10 14:49:06 +0700

    ft_strcmp
    now works w/ non-ascii characters

--- scrap/ft_strstr.c
+++ scrap/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/09 20:04:34 by sadamant          #+#    #+#             */
-/*   Updated: 2017/09/09 20:07:53 by sadamant         ###   ########.fr       */
+/*   Updated: 2017/09/10 10:35:45 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,23 @@
 
 char *ft_strstr(const char *haystack, const char *needle)
 {
-	if (*haystack == *needle)
+	size_t i;
+
+	i = 0;
+	while (*haystack)
 	{
+		if (*haystack++ == *needle++)
+		{
+			while(i < ft_strlen(needle))
+			{
+				if (haystack[i++] == *needle++)
+					;
+				else
+					return NULL;
+			}
+			return ((char	*)haystack);
+		}
+	}
 	
-	}
-	else
-	{
-		haystack++;
-	}
 	return NULL;
 }
