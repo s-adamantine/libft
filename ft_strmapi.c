d18006e218b224a6d1a184c59a6e306e208c1d63
sadamant <sharfy.adamantine@gmail.com>
2017-09-10 20:26:31 +0700

    ft_strmapi
    works but unprotected

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/10 20:23:18 by sadamant          #+#    #+#             */
+/*   Updated: 2017/09/10 20:26:11 by sadamant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char)*(ft_strlen(s) + 1));
+	if (str)
+	{
+		while (*s)
+		{
+			str[i] = f(i, *s++);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return NULL;
+}
