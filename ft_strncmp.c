620b3bd33c6fca5f70282db92e790c9681041fba
sadamant <sharfy.adamantine@gmail.com>
2017-09-14 21:54:29 +0700

    ft_strncmp
    passes all tests

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/19 16:31:43 by sadamant          #+#    #+#             */
-/*   Updated: 2017/08/19 17:09:16 by sadamant         ###   ########.fr       */
+/*   Created: 2017/09/14 21:43:10 by sadamant          #+#    #+#             */
+/*   Updated: 2017/09/14 21:53:27 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while(*s1 != '\0' && *s2 != '\0')
-		if (*s1++ > *s2++)
-			return (1);
-		else
+	int i;
+
+	i = 0;
+	if (ft_strlen(s1) < ft_strlen(s2))
+		return (-1);
+	while (n-- > 0 && s1[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else if ((unsigned long)s1[i] < (unsigned long)s2[i])
 			return (-1);
+		else
+			return (1);
+	}
 	return (0);
 }
