9b8170b93729499f555a7948743e19e7faa7765f
Sharfina Adamantine <sadamant@student.42.us.org>
2017-10-01 21:46:41 -0700

    ft_strnequ
    fixup

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/09 18:08:20 by sadamant          #+#    #+#             */
-/*   Updated: 2017/09/09 18:11:36 by sadamant         ###   ########.fr       */
+/*   Updated: 2017/10/01 21:45:40 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,18 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (ft_strlen(s1) == ft_strlen(s2))
 	{
-		while (n-- > 0)
+		while (n--)
 		{
 			if (s1[i] == s2[i])
 			{
 				i++;
 			}
-			return (1);
+			else
+			{
+				return (0);
+			}
 		}
+		return (1);
 	}
 	return (0);
 }
