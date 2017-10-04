138cb5bdbc83632ea13dc48f6a07fa3ae688f4f4
sadamant <sharfy.adamantine@gmail.com>
2017-10-04 00:43:35 -0700

    ft_strnstr
    fixed failed case where needle is larger than len

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,22 +6,23 @@
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 15:54:49 by sadamant          #+#    #+#             */
-/*   Updated: 2017/10/02 15:11:50 by sadamant         ###   ########.fr       */
+/*   Updated: 2017/10/04 00:40:01 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *small, size_t len)
 {
-	size_t	n;
+	int	n;
 
 	n = len - ft_strlen(small) + 1;
 	if (ft_strcmp(small, "") == 0)
 	{
 		return ((char *)big);
 	}
-	while (*big && n--)
+	while (*big && n-- > 0)
 	{
 		if (*big == *small)
 		{
