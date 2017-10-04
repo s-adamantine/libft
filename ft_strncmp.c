b35c2973799453f140880926e8a9476711d41523
sadamant <sharfy.adamantine@gmail.com>
2017-10-03 22:54:37 -0700

    ft_strncmp
    fixup

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/14 21:43:10 by sadamant          #+#    #+#             */
-/*   Updated: 2017/09/14 21:53:27 by sadamant         ###   ########.fr       */
+/*   Updated: 2017/10/03 22:53:02 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int i;
 
 	i = 0;
-	if (ft_strlen(s1) < ft_strlen(s2))
-		return (-1);
-	while (n-- > 0 && s1[i])
+	while (n--)
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] == s2[i] && s1[i] && s2[i])
 			i++;
-		else if ((unsigned long)s1[i] < (unsigned long)s2[i])
-			return (-1);
 		else
-			return (1);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	}
 	return (0);
 }
