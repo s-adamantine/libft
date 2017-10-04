3178677305c16cc5419134a9e010497a96e4b1b9
sadamant <sharfy.adamantine@gmail.com>
2017-10-04 00:23:48 -0700

    ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/29 21:06:01 by sadamant          #+#    #+#             */
-/*   Updated: 2017/09/30 17:26:47 by sadamant         ###   ########.fr       */
+/*   Updated: 2017/10/04 00:21:40 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	return (f(lst));
+	t_list	*head;
+	t_list	*current;
+	t_list	*temp;
+
+	temp = f(lst);
+	current = ft_lstnew(temp->content, temp->content_size);
+	head = current;
+	lst = lst->next;
+	while (lst)
+	{
+		temp = f(lst);
+		current->next = ft_lstnew(temp->content, temp->content_size);
+		current = current->next;
+		lst = lst->next;
+	}
+	return (head);
 }
