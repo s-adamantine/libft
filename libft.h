f0ae5b9d9e4b62fb33fa84ad03a4eb0ba144c309
Sharfina Adamantine <sadamant@student.42.us.org>
2017-10-11 13:37:29 -0700

    ft_strcchr
    function returns count of chr in str

--- libft.h
+++ libft.h
@@ -76,6 +76,7 @@ int					ft_isascii(int c);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
 int					ft_isprint(int c);
+int					ft_strcchr(char *s, int c);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strequ(char const *s1, char const *s2);
